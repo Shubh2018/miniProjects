4f1f8b730c53c6a8903ea07562f2c4f192af0fce
Shubh2018 <shubhendrachaddha.16@gmail.com>
2020-04-11 10:12:12 +0530

    Complteted

--- TextBasedAdventureGame.py
+++ TextBasedAdventureGame.py
@@ -1,39 +1,30 @@
+import random
+
 ###Player Class
 
 class Player:
-    def __init__(self, nickName, health=100, hasSword=False, hasShield=False, powerUps = [], notDead=True):
+    def __init__(self, nickName, health=100, hasSword=False, hasShield=False, notDead=True, restartGame="y"):
         self.nickName = nickName
-        self.health = 100
+        self.health = 40
         self.hasSword = hasSword
         self.hasShield = hasShield
-        self.powerUps = powerUps
         self.notDead = notDead
+        self.restartGame = restartGame
 
     def showStats(self):
         print("Health: ", self.health)
         if self.hasShield is True and self.hasSword is True:
-            print("Inventory:")
+            print("\nInventory:")
             print("1. Sword")
-            print("2. Shield")
+            print("2. Shield\n")
         elif self.hasShield is True and self.hasSword is False:
-            print("Inventory:")
-            print("1.Shield")
+            print("\nInventory:")
+            print("1.Shield\n")
         elif self.hasShield is False and self.hasSword is True:
-            print("Inventory:")
-            print("1.Sword")
+            print("\nInventory:")
+            print("1.Sword\n")
         else:
-            print("Inventory is Empty")
-
-        print("PowerUps Used: ")
-
-        for i in self.powerUps:
-            if i is "health":
-                print("health: Restores Health")
-            elif i is "oneHitKill":
-                print("oneHitKill: Kills enemy with one hit, can be used only once with no health loss")
-            else:
-                print("No Power Ups used yet!")
-                break
+            print("\nInventory is Empty\n")
         
     def getWeapons(self):
         self.hasSword = True
@@ -42,78 +33,137 @@ class Player:
         # print(self.hasShield)
 
     def decHealth(self):
-        self.health -= 5
+        lostHealth = random.randint(1, 21)
+        self.health -= lostHealth
 
         if(self.health < 1):
-            self.Dead()
+            self.Dead(self.restartGame)
 
     def attackEnemy(self):
         self.decHealth()
 
-    def powerUp(self, pickUps):
-        if pickUps is "health":
-            self.powerUps.append(pickUps)
-            self.health = 100
-        elif pickUps is "oneHitKill":
-            self.powerUps.append(pickUps)
+        self.showStats()
 
-    def Dead(self):
+    def powerUp(self):
+        self.health += 10
+        if self.health > 100:
+            self.health = 100
+
+    def Dead(self, restartGame):
         self.notDead = False
+
+        print("\nRestart Game?(y/n): ")
+        self.restartGame = input()
+
+        if self.restartGame == "y" or self.restartGame == "Y":
+            self.notDead = True
+        else:
+            exit()
+
+        print()
     
     def isDead(self):
         return self.notDead
 
-
 def Game():
-    print("Enter Name: ")
+    print("\nEnter Name: ")
     name = input()
     player = Player(name)
 
-    print("Rules:-")
+    print("\nRules:-")
     print("Press number keys to choose the options")
-    print("Press i to see the inventory and the power ups used")
     print("Press ctrl + z at any time to quit")
-    print("Press 's' to start")
+    print("Press 's' to start\n")
 
     keyPress = input()
+    restartGame = "y"
 
     if keyPress == "s":
-        while (player.isDead()):
+        while player.isDead():
             print("You are in a Dungeon of a Destroyd Castle. There is a door infront of you.") 
             print("You open the door and go out of the room. There is a room to your right and a corridor in front of you. What do you do?")
             print("1. Open the Door")
             print("2. Move Forward")
 
             choice = input()
+
             if choice == "2":
-                print("You move forward. A hoard of monsters chase you away. Game Over!")
-                player.Dead()
+                print("\nYou move forward. A hoard of monsters chase you away. Game Over!\n")
+                player.Dead(restartGame)
 
             elif choice == "1":
                 print("You Enter the room. There is a chest inside. What do you do?")
                 print("1. Open the Chest")
                 print("2. Leave the room and continue")
 
-                choice1 = input()
-                if choice1 == "2":
-                    print("You leave the room and continue the journey. A hoard of monsters chase you away. Game Over!")
-                    player.Dead()
+                choice = input()
+                if choice == "2":
+                    print("You leave the room and continue the journey. A hoard of monsters chase you away. Game Over!\n")
+                    player.Dead(restartGame)
 
-                elif choice1 == "1":
-                    print("You open the chest and find a sword and a shield. You pick up both and continue the journey.")
+
+                elif choice == "1":
+                    print("You open the chest and find a sword and a shield. You pick up both and continue the journey.\n")
                     player.getWeapons()
 
-                    print("You come accross a hoard of monsters. You kill them with your weapons, but lose some health.")
+                    print("You come accross a hoard of monsters. You kill them with your weapons, but lose some health. The enemies are dead(d.\n")
                     player.attackEnemy()
-                    break
-                    
-                elif choice1 == "i":
-                    player.showStats()
+                    print("You Continue walking forward. You come across a junction.")
+                    print("One path goes to the left, other staright and the other one towards right. Which path do yo take?\n")
+                    print("1. Go Left")
+                    print("2. Go Straight")
+                    print("3. Go Right")
+
+                    choice = input()
+
+                    if choice == "1":
+                        print("\nYou decide to turn Left. You are going straight, when you step on an unusual stone.")
+                        print("Its a trap! You try to escape, but you come under a big boulder. Game Over!!!\n")
+                        player.Dead(restartGame)
+    
+
+                    elif choice == "2":
+                        print("\nYou decide to go straight. You come across a hoard of monsters. You fight and stands victorious, but lose some health.\n")
+                        player.attackEnemy()
+
+                        print("There is door in front of You. You go inside. There is a chest in front of you. You open it.")
+                        print("You completed the objective for which you came here. You win!!")
+                        break
             
-            elif choice == "i":
-                player.showStats()
+                    elif choice == "3":
+                        print("\nYou turn right. You find a Health powerup!!!")
+                        print("You go back. What do you do?")
+
+                        player.powerUp()
+                        player.showStats()
+
+                        print("1. Go Left")
+                        print("2. Go Straight")
+
+                        choice = input()
+
+                        if choice == "1":
+                            print("\nYou decide to turn Left. You are going straight, when you step on an unusual stone.")
+                            print("Its a trap! You try to escape, but you come under a big boulder. Game Over!!!\n")
+                            player.Dead(restartGame)
+        
+
+                        elif choice == "2":
+                            print("\nYou decide to go straight. You come across a hoard of monsters. You fight and stand victorious, but lose some health.\n")
+                            player.attackEnemy()
+
+                            print("There is door in front of You. You go inside. There is a chest in front of you. You open it.")
+                            print("You completed the objective for which you came here. You win!!")
+                            break
+
+
 
 def main():
+    print(r"|````| |````\  \         / |`````` |`\     | ```|``` |     | |```\  |``````   |`````` |````| |^\     /^| |``````")
+    print(r"|    | |     |  \       /  |       |  \    |    |    |     | |    | |         |       |    | |  \   /  | |      ")
+    print(r"|----| |     |   \     /   |----   |   \   |    |    |     | |  _/  |----     |   __  |----| |   \_/   | |----  ")
+    print(r"|    | |     |    \   /    |       |    \  |    |    |     | |  \   |         |     | |    | |         | |      ")
+    print(r"|    | |____/      \_/     |______ |     \_|    |    |_____| |   \  |______   |_____| |    | |         | |______")
     Game()
 
 main()
